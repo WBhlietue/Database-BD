a5fe169fd4f4e8c3c8cd9344c139e93e3bc1220f
WBhlietue <WBhlietue@gmail.com>
2022-12-08 22:52:46 +0800

    tailan, tsagiin huvaari harah

--- BDTailan5.sql
+++ BDTailan5.sql
@@ -1,1 +1,24 @@
 -- Ajilchdiin tsagiin huvaari
+
+create proc GetTimeTable
+    (@empCode char(7))
+as
+begin
+    select @empCode as 'Code', (
+    select top 1
+            dep_code
+        from
+            EmployeeRegister
+            join EmpReg on EmployeeRegister.empReg_code = EmpReg.empReg_code
+        where emp_code = @empCode
+) as 'Depart'
+    select empReg_date, timeTable
+    from EmployeeRegister
+        join EmpReg on EmployeeRegister.empReg_code = EmpReg.empReg_code
+    where emp_code = @empCode
+end
+
+use DB;
+
+
+exec GetTimeTable 'U001001'
