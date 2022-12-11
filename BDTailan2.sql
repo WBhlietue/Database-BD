1b1756440980f2d4c47e60b2e4c988df131b916d
Badral <75673666+Badral0726@users.noreply.github.com>
2022-12-11 17:56:57 +0800

    Update BDTailan2.sql

--- BDTailan2.sql
+++ BDTailan2.sql
@@ -1,1 +1,13 @@
--- Niiluuleltiin tuhai tailan
+USE DB
+
+CREATE PROCEDURE NIILUULELTIIN_TAILAN 
+(@SUPP_NAME VARCHAR(50), @ORDER_DATE CHAR(10), @Dep_code CHAR(3))
+AS
+BEGIN
+SELECT SupponceOrderRegister.ITEM_CODE , SupponceOrderRegister.num
+FROM SupponceOrderRegister
+JOIN SupOrder ON SupOrder.order_num = SupponceOrderRegister.order_num
+WHERE SupOrder.suppName = @SUPP_NAME AND SupOrder.order_date = @ORDER_DATE
+AND SupOrder.dep_code = @Dep_code
+END
+GO;
