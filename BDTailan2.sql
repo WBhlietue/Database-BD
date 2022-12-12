907b750959b7d801ce468301358f993305f11e76
WBhlietue <WBhlietue@gmail.com>
2022-12-12 13:27:22 +0800

    fix tailan2

--- BDTailan2.sql
+++ BDTailan2.sql
@@ -1,5 +1,6 @@
-USE DB
+USE DB;
 
+go
 CREATE PROCEDURE NIILUULELTIIN_TAILAN 
 (@SUPP_NAME VARCHAR(50), @ORDER_DATE CHAR(10), @Dep_code CHAR(3))
 AS
@@ -7,7 +8,10 @@ BEGIN
 SELECT SupponceOrderRegister.ITEM_CODE , SupponceOrderRegister.num
 FROM SupponceOrderRegister
 JOIN SupOrder ON SupOrder.order_num = SupponceOrderRegister.order_num
-WHERE SupOrder.suppName = @SUPP_NAME AND SupOrder.order_date = @ORDER_DATE
+join ItemOr on ItemOr.itemOrderCode = SupOrder.itemOrderCode
+WHERE ItemOr.suppName = @SUPP_NAME AND SupOrder.order_date = @ORDER_DATE
 AND SupOrder.dep_code = @Dep_code
 END
 GO;
+
+exec NIILUULELTIIN_TAILAN 'Speedwagon Foundation', '2022/12/08', '001'
