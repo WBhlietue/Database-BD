794b31c0fa1af323f6627abf643604961bb0f73c
WBhlietue <WBhlietue@gmail.com>
2022-12-13 11:42:40 +0800

    tailan 7

--- BDTailan7.sql
+++ BDTailan7.sql
@@ -3,61 +3,51 @@ use DB;
 go
 create view CustomerZahialga
 as
-    select SupOrder.order_num, item_name, SupponceOrderRegister.ITEM_CODE , SupponceOrderRegister.num, (SupponceOrderRegister.num * Items.item_price) as 'Total', suppName, SupOrder.order_date, SupOrder.dep_code
-    from SupponceOrderRegister
-        join SupOrder on SupOrder.order_num = SupponceOrderRegister.order_num
-        join ItemOr on ItemOr.itemOrderCode = SupOrder.itemOrderCode
-        join Items on SupponceOrderRegister.item_code = Items.item_code
+    select CusOrder.order_num, item_name, CustomerOrderRegister.ITEM_CODE , CustomerOrderRegister.num, (CustomerOrderRegister.num * Items.item_price) as 'Total', CusOrder.order_date, CusOrder.dep_code, Employee.emp_code, emp_fName
+    from CustomerOrderRegister
+        join CusOrder on CusOrder.order_num = CustomerOrderRegister.order_num
+        join Items on CustomerOrderRegister.item_code = Items.item_code
+        join Employee on Employee.emp_code = CusOrder.emp_code
 
 go
-create procedure NIILUULELTIIN_TAILAN
-    (@SUPP_NAME VARCHAR(50),
-    @ORDER_DATE CHAR(10),
-    @Dep_code CHAR(3))
-as
-begin
-    select @SUPP_NAME as 'supp Name', @ORDER_DATE as 'date', @Dep_code as 'depCode'
-    select item_name, item_code , num, Total
-    from Niiluulelt
-    where suppName = @SUPP_NAME and order_date = @ORDER_DATE
-        and dep_code = @Dep_code
-
-    select sum(Total) as 'Total'
-    from Niiluulelt
-    where suppName = @SUPP_NAME and order_date = @ORDER_DATE
-        and dep_code = @Dep_code
-end
-go
-create procedure NIILUULELTIIN_TAILANallSupp
+create procedure CustomerZahialgaTailan
     (
     @ORDER_DATE CHAR(10),
     @Dep_code CHAR(3))
 as
 begin
     select @ORDER_DATE as 'date', @Dep_code as 'depCode'
-    select suppName, item_name, item_code , num, Total
-    from Niiluulelt
+    select order_num, emp_code, item_name, item_code , num, Total
+    from CustomerZahialga
     where  order_date = @ORDER_DATE
         and dep_code = @Dep_code
 
+    select sum(Total) as 'Total'
+    from CustomerZahialga
+    where order_date = @ORDER_DATE
+        and dep_code = @Dep_code
 end
 
+
 go
-create procedure NiluuleltTailanWithDugaar
+create procedure CustomerZahialgaWithDugaar
     (@supCode char(15))
 as
 begin
     select top 1
-        suppName, dep_code, order_date
-    from Niiluulelt
+        emp_code, dep_code, order_date, order_num
+    from CustomerZahialga
     where order_num=@supCode
 
     select item_name, item_code , num, Total
-    from Niiluulelt
+    from CustomerZahialga
+    where order_num=@supCode
+
+    select sum ( num* Total) as Total
+    from CustomerZahialga
     where order_num=@supCode
 end
 
 
-exec NIILUULELTIIN_TAILAN 'Speedwagon Foundation', '2022/12/08', '001'
-exec NIILUULELTIIN_TAILANallSupp  '2022/12/08', '001'
-exec NiluuleltTailanWithDugaar 'N20221209001001'
+exec CustomerZahialgaTailan  '2022/12/08', '001'
+exec CustomerZahialgaWithDugaar 'Z20221209001001'
